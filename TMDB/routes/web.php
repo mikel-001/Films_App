170dcf96560d6cdaffab4e8aab975671b1395db7
Rida <ridaelbouhafsi1@gmail.com>
2024-09-04 09:53:50 +0100

    last commit 2

--- TMDB/routes/web.php
+++ TMDB/routes/web.php
@@ -26,5 +26,5 @@ Route::resource('films', ListFilmsController::class)->middleware([
     'verified',
 ]);
 
-// le lien utiliser pour la methode de recherche 
+// le lien utiliser pour gerer la methode de recherche 
 Route::get('/search_film', [ListFilmsController::class, 'search'])->name("search_film");
